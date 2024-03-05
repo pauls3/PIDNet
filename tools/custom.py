5dc62a8b44e2c8ce48c84a74fc803b244e229bbe
pauls3 <paulstanik3@gmail.com>
2024-03-04 16:44:21 -0800

    updated path for samples

--- tools/custom.py
+++ tools/custom.py
@@ -42,7 +42,7 @@ def parse_args():
     parser.add_argument('--a', help='pidnet-s, pidnet-m or pidnet-l', default='pidnet-l', type=str)
     parser.add_argument('--c', help='cityscapes pretrained or not', type=bool, default=True)
     parser.add_argument('--p', help='dir for pretrained model', default='../pretrained_models/cityscapes/PIDNet_L_Cityscapes_test.pt', type=str)
-    parser.add_argument('--r', help='root or dir for input images', default='../samples/', type=str)
+    parser.add_argument('--r', help='root or dir for input images', default='samples/', type=str)
     parser.add_argument('--t', help='the format of input images (.jpg, .png, ...)', default='.png', type=str)     
 
     args = parser.parse_args()
