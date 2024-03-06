c11af63517ad29134b054b962f44400211fd7fe5
pauls3 <paulstanik3@gmail.com>
2024-03-05 20:42:07 -0800

    finetuning with cityscapes

--- datasets/flame.py
+++ datasets/flame.py
@@ -41,14 +41,13 @@ class Flame(BaseDataset):
         self.files = self.read_files()
         self.ignore_label = ignore_label
 
-        self.class_weights = torch.FloatTensor([0.7, 2.0]).cuda()
+        # self.class_weights = torch.FloatTensor([0.7, 2.0]).cuda()
+        self.class_weights = None
 
         self.label_mapping = {-1: ignore_label, 0: 0, 
                         1: 1}
 
         self.color_list = [[255,255,255], [0,0,0]]
-        
-        # self.class_weights = None
 
         self.bd_dilate_size = bd_dilate_size
     
